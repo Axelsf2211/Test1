44d41d17792b95bc56c966cd18570590aedcf6dc
Axelsf2211 <axel.sangle-ferriere@etu.unilasalle.fr>
2025-03-26 15:50:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-import streamlit as st
-st.write("Bonjour, bienvenue dans la galerie privée de Camille Caussel")
+import streamlit as st 
+st.markdown(
+    "<h1 style='font-size:30px; font-family:Arial; color:blue;'>Bienvenue dans la galerie privée de Camille Caussel</h1>", 
+    unsafe_allow_html=True
+)
 st.write("photos de charmes accessibles gratuitement")
